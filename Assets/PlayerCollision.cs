6c4c95b19174f8c0398684c422ae7813870f38ea
kunaalkumar <kunaal@ashiana.com>
2020-01-11 17:17:00 -0700

    Initial commit

---
+++ Assets/PlayerCollision.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+
+    public PlayerMovement movementScript;
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+       if(!collisionInfo.collider.tag.Equals("Ground")) {
+           Debug.Log("Collided with " + collisionInfo.collider.name);
+           movementScript.enabled = false;
+       }
+    }
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody playerBody;
+    public float movementSpeed = 2000f;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        if (Input.GetKey("w"))
+        {
+            playerBody.AddForce(0, 0, movementSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey("a"))
+        {
+            playerBody.AddForce(-movementSpeed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("s"))
+        {
+            playerBody.AddForce(0, 0, -movementSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey("d"))
+        {
+            playerBody.AddForce(movementSpeed * Time.deltaTime, 0, 0);
+        }
+    }
+
+}
